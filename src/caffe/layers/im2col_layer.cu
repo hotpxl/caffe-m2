b4928be8bd4c895ef9e506c8a02fd3546b470b9a
Yutian Li <kevinleeone@gmail.com>
2014-05-08 14:50:07 +0800

    [master] Crashes run

--- src/caffe/layers/im2col_layer.cu
+++ src/caffe/layers/im2col_layer.cu
@@ -14,10 +14,8 @@ void Im2colLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = (*top)[0]->mutable_gpu_data();
-  for (int n = 0; n < bottom[0]->num(); ++n) {
-    im2col_gpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, PAD_, STRIDE_, top_data + (*top)[0]->offset(n));
-  }
+  im2col_gpu(bottom_data, bottom[0]->num(), CHANNELS_, HEIGHT_,
+      WIDTH_, KSIZE_, PAD_, STRIDE_, top_data);
 }
 
 template <typename Dtype>
@@ -25,10 +23,8 @@ Dtype Im2colLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const bool propagate_down, vector<Blob<Dtype>*>* bottom) {
   const Dtype* top_diff = top[0]->gpu_diff();
   Dtype* bottom_diff = (*bottom)[0]->mutable_gpu_diff();
-  for (int n = 0; n < top[0]->num(); ++n) {
-    col2im_gpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
-  }
+  col2im_gpu(top_diff, top[0]->num(), CHANNELS_, HEIGHT_,
+      WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff);
   return Dtype(0.);
 }
 
